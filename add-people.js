c57d6c09a0cb7aeeab5297b31d9eef6549408e1d
Daniela Alves <dani.alvesm22@gmail.com>
2023-09-06 13:55:27 -0300

    Create add-people.js

---
+++ add-people.js
@@ -0,0 +1,19 @@
+function addPeople() {
+  let infoClasses = Classroom.Courses.list().courses;
+
+  infoClasses = infoClasses.filter(c => (c.courseState === 'ACTIVE' && c.section === '12 / 2023'));
+
+  infoClasses.map((c) => {
+    try {
+      Classroom.Invitations.create({
+        "courseId": c.id,
+        "userId": "2019500145@aluno.lfb.g12.br",
+        "role": "STUDENT"
+      })
+      Logger.log("Sucesso ao adicionar na turma: " + c.name + " - " + c.section)
+    } catch(e) {
+      Logger.log(e)
+      Logger.log("Erro ao adicionar")
+    }
+  })
+}
